bf54832f6e9e2fee1d37ebaf039ebcde5150d4d5
RefilweMk3 <refilwekgwadi13@gmail.com>
2024-09-25 17:40:28 +0200

    File Handling and text appending in py

--- main.py
+++ main.py
@@ -9,3 +9,13 @@ file.close()
 file=open("web.txt","a")
 file.write("\n Heres a new python portal")
 file.close()
+
+
+
+f1=open("ref.txt","a+")
+f2=open("kg.txt","r")
+f1.write(f2.read())
+f1.seek(0)
+f2.seek(0)
+print(f1.read())
+print(f2.read())
